f5dcd78b7bd9e74d3b036ed802600b460fdcb95e
Steven Shidi Zhou <stevenshidizhou@localhost.localdomain>
2020-12-12 23:24:03 +0100

    wip

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from torchvision import models
 from torch import nn
+import torch
+from torch.autograd import Variable
 
 
 class Encoder(nn.Module):
@@ -15,10 +17,78 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         pretrained_model = models.resnet34(pretrained=True)
         self.resnet = nn.Sequential(*list(pretrained_model.children())[:-1])
-        self.linear = nn.Linear(pretrained_model.fc.in_features, out_features=1000)
-        self.batchnorm = nn.BatchNorm1d(num_features=1000, momentum=0.01)
+        self.linear = nn.Linear(pretrained_model.fc.in_features, out_features=512)
+        self.batchnorm = nn.BatchNorm1d(num_features=512, momentum=0.01)
 
         # init weights randomly with normal distribution
-        nn.init.xavier_normal(self.linear.weight, gain=1)
+        nn.init.xavier_normal_(self.linear.weight, gain=1)
         nn.init.constant_(self.linear.bias, 0)
+
+    def forward(self, x):
+        """
+        Forward propagation.
+        @param
+            images: images, a tensor of dimensions
+        return:
+            image embeddings:
+                else: (batch_size, embed_size)
+        """
+        x = self.resnet(x)
+        x = Variable(x.data)
+        x = x.view(x.size(0), -1)  # flatten
+        x = self.linear(x)
+
+        return x
+
+
+class Decoder(nn.Module):
+    """
+    Decoder: use RNN with LSTM
+    """
+    def __init__(self, vocab_size, embed_size, hidden_size, num_layers=1):
+        super(Decoder, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions, lengths):
+        """
+        Forward pass through the network
+        :param features: features from CNN feature extractor
+        :param captions: encoded and padded (target) image captions
+        :param lengths: actual lengths of image captions
+        :returns: predicted distributions over the vocabulary
+        """
+
+        # embed tokens in vector space
+        embeddings = self.embed(captions)
+        # append image as first input
+        #print(features.size()) torch.Size([128, 1000])
+
+        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+
+        # pack data (prepare it for pytorch model)
+        packed = nn.utils.rnn.pack_padded_sequence(inputs, lengths, batch_first=True)
+
+        # run data through recurrent network
+        hiddens, _ = self.lstm(packed)
+        # return hiddens
+        # hiddens here is a PackedSequence, hiddens[0] is the data tensor
+        # hiddens[1] is the batch_size tensor
+        outputs = self.linear(hiddens[0])
+        return outputs
+
+    def sample(self, features, states=None):
+        """Generate captions for given image features using greedy search."""
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_seg_length):
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
+        return sampled_ids
 
--- train.py
+++ train.py
@@ -1,25 +1,94 @@
-from model import Encoder
+import pickle
+import torch.utils.data as data
+
+from model import Encoder, Decoder
 from torchvision import models
+from vocab import Vocabulary
+from torch import nn
+import torch
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import numpy as np
+import os
+import data_func
 
+#params
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+learning_rate = 1e-3
+epochs = 3
+log_step = 10
+save_step = 1000
+batch_size = 128
+num_workers = 2
 
 data_transform = transforms.Compose([
-        transforms.RandomCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
 
-cap = dset.CocoCaptions(root='./coco/images/train2014',
-                        annFile='./coco/annotations/captions_train2014.json',
-                        transform=data_transform)
+# Load vocabulary wrapper
+with open('/home/stevenshidizhou/KTH/lab2/coco/vocab.pkl', 'rb') as f:
+    vocab = pickle.load(f)
 
-print('Number of samples: ', len(cap))
-img, target = cap[3] # load 4th sample
+#cap = dset.CocoCaptions(root='./coco/images/train2014',
+#                        annFile='./coco/annotations/captions_train2014.json',
+#                        transform=transforms)
 
-print("Image Size: ", img.size())
-print(img)
-print(target)
-#Encoder()
+data_loader = data_func.get_loader(root='./coco/images/resized2014',
+                                   json='./coco/annotations/captions_train2014.json',
+                                   vocab=vocab,
+                                   transform=data_transform,
+                                   batch_size=batch_size,
+                                   shuffle=True,
+                                   num_workers=num_workers
+                                   )
+# Encoder & Decoder init
+encoder = Encoder().to(device)
+decoder = Decoder(vocab_size=len(vocab),
+                  embed_size=512,
+                  hidden_size=512,
+                  num_layers=1).to(device)
+
+# Loss and Optimizer
+lossFn = nn.CrossEntropyLoss()
+params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.batchnorm.parameters())
+optimizer = torch.optim.Adam(params, lr=learning_rate)
+
+#Train the model
+total_step = len(data_loader)
+
+for epoch in range(epochs):
+    for i, (images, captions, lengths) in enumerate(data_loader):
+        #lengths = captions
+
+        # Set mini-batch dataset
+        images = images.to(device)
+        captions = captions.to(device)
+        targets = nn.utils.rnn.pack_padded_sequence(captions, lengths, batch_first=True)[0]
+
+        # Forward, backward and optimize
+        features = encoder(images)
+        outputs = decoder(features, captions, lengths)
+        loss = lossFn(outputs, targets)
+        decoder.zero_grad()
+        encoder.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+        # Print log info
+        if i % log_step == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
+                  .format(epoch, epochs, i, total_step, loss.item(), np.exp(loss.item())))
+
+            # Save the model checkpoints
+        if (i + 1) % save_step == 0:
+            torch.save(decoder.state_dict(), os.path.join(
+                'models/', 'decoder-{}-{}.ckpt'.format(epoch + 1, i + 1)))
+            torch.save(encoder.state_dict(), os.path.join(
+                'models/', 'encoder-{}-{}.ckpt'.format(epoch + 1, i + 1)))
